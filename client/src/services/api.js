8830094c733cb038b477d4fcdea58410181ebb08
Drumblow <fabricappsdrumblow@gmail.com>
2024-11-15 10:10:45 -0500

    Update api.js Configuration

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fun-english-backend.onrender.com/api',
+  baseURL: 'https://fun-english.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },

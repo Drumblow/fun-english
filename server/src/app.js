87e350cf96c1f69e652fda594a7173fff0b70392
Drumblow <fabricappsdrumblow@gmail.com>
2024-11-15 09:04:25 -0500

    Update Cors configuration

--- server/src/app.js
+++ server/src/app.js
@@ -11,22 +11,25 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+ origin: [
+   'http://localhost:5173',
+   'https://funny-lolly-1a3a84.netlify.app'
+ ],
+ methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+ allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/lessons', require('./routes/lessonRoutes'));
-app.use('/api/exercises', require('./routes/exerciseRoutes')); // Nova linha
+app.use('/api/lessons', require('./routes/lessonRoutes')); 
+app.use('/api/exercises', require('./routes/exerciseRoutes'));
 
 // Error handling
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something broke!', error: err.message });
+ console.error(err.stack);
+ res.status(500).json({ message: 'Something broke!', error: err.message });
 });
 
 module.exports = app;

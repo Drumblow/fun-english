90e9047768deed919d6960ecde92fe82a0988b67
Drumblow <fabricappsdrumblow@gmail.com>
2024-11-14 23:59:41 -0500

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://fun-english-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
